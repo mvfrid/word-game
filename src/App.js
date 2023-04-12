d07ce81ff466b53f7d75d6fc5ad1e5c34cbc6c75
Frid Matilda <matilda.x.frid@scania.com>
2023-04-13 00:15:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { MainButton } from './components/styled_components/buttons.js'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="background">
+      <div className="welcome">
+        <h2>Welcome to Word Game!</h2>
+        <MainButton>Click me</MainButton>
+      </div>
     </div>
   )
 }
---
+++ src/components/styled_components/buttons.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const MainButton = styled.button`
+  background-color: blue;
+  color: white;
+  border-radius: 25px;
+  cursor: pointer;
+  padding: 7px;
+  border: none;
+
+  &:hover {
+    box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
+  }
+`;
